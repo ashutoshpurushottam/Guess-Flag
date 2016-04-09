f9467ec59a16af895093271655636e6e8307c73b
ashutoshpurushottam <apurushottam@icloud.com>
2016-04-09 19:21:04 +0530

    Choose random flags and show title for a random choice

--- Guess-Flag/ViewController.swift
+++ Guess-Flag/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
     
@@ -16,30 +17,41 @@ class ViewController: UIViewController {
     
     var score = 0
     var countries = [String]()
+    var correctAnswer = 0
     
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
-        // Set image border on flag images
+        // Set border on flag images
         topFlagButton.layer.borderWidth = 1
         centerFlagButton.layer.borderWidth = 1
         bottomFlagButton.layer.borderWidth = 1
         topFlagButton.layer.borderColor = UIColor.lightGrayColor().CGColor
         centerFlagButton.layer.borderColor = UIColor.lightGrayColor().CGColor
         bottomFlagButton.layer.borderColor = UIColor.lightGrayColor().CGColor
+        
         askQuestions()
     }
     
     func askQuestions()
     {
+        // Shuffle array
+        GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries)
+        
         topFlagButton.setImage(UIImage(named: countries[0]), forState: .Normal)
         centerFlagButton.setImage(UIImage(named: countries[1]), forState: .Normal)
         bottomFlagButton.setImage(UIImage(named: countries[2]), forState: .Normal)
         
+        // Generate random number between 0 and 2
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        
+        // Set title 
+        title = countries[correctAnswer].uppercaseString
+        
     }
-
+    
 
 }
 
